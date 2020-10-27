8cdf8f26eebc69062e26cba5aeef0307a415b9a0
mritd <mritd@linux.com>
2020-10-27 13:34:30 +0800

    feat(openjdk): add push
    add push
    
    Signed-off-by: mritd <mritd@linux.com>

--- openjdk/build.sh
+++ openjdk/build.sh
@@ -11,6 +11,7 @@ function build(){
     for base_image in 8-jdk-hotspot 8-jdk-openj9 11-jdk-hotspot 11-jdk-openj9; do
         info "building adoptopenjdk ${base_image}..."
         sed "s@{{BASE_IMAGE}}@adoptopenjdk:${base_image}@g" Dockerfile | docker build -t ytpay/adoptopenjdk:${base_image} -f - . ${CACHE_CMD}
+        docker push ytpay/adoptopenjdk:${base_image}
     done
 }
 
